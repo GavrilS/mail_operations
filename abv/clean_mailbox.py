ed78a680080225e6ad5c6bf55c4199c8905a1eac
GavrilS <gavr1lll@abv.bg>
2026-07-03 20:31:54 +0300

    Adding the abv connector class to connect to email box -> wip

--- abv/clean_mailbox.py
+++ abv/clean_mailbox.py
@@ -1,6 +1,12 @@
 '''
 This script is handling connecting to an abv.bg mail box, retrieving X number of emails and 
 archiving them.
+
+Functionality:
+    - connect to email box
+    - retrieve assigned number of messages
+    - save them for future analysis
+    - archive the messages in the mail box
 '''
 import os
 import poplib
@@ -9,3 +15,8 @@ USER = os.getenv('ABV_USER')
 PASS = os.getenv('ABV_PASSWORD')
 POP_SERVER = os.getenv('ABV_POP_SERVER')
 POP_PORT = os.getenv('ABV_POP_PORT')
+
+class ABVConnector:
+
+    def __init__(self):
+        pass
