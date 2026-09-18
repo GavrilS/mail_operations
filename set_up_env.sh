22f34f5e770e250eb8c6b3abe6d00f07d759ddb9
GavrilS <gavr1lll@abv.bg>
2026-09-18 23:10:42 +0300

    Added a script to set up python venv and install required libraries

---
+++ set_up_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [[ ! -d ./env ]]; then
+    echo "Environment is missing so setting it up now"
+    python3 -m venv env/
+fi
+
+echo "Installing python dependencies"
+
+source env/bin/activate
+
+python3 -m pip install -r requirements.txt
+
+deactivate
