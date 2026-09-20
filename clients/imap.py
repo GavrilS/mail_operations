5a5f10966501c4f7a5da6e5c07bb8acbd7c4f5ae
GavrilS <gavr1lll@abv.bg>
2026-09-20 08:43:24 +0300

    Adding functionality to extract message ids from the imap server -> WIP

--- clients/imap.py
+++ clients/imap.py
@@ -3,9 +3,11 @@ import email
 from imapclient import IMAPClient
 
 IMAP_CLIENT_DEFAULTS = {
-    'email': os.getenv('GMAIL_ACC', ''),
-    'password': os.getenv('GMAIL_APP_PASS', '')
+    'email': os.getenv('IMAP_ACC', ''),
+    'password': os.getenv('IMAP_PASS', ''),
+    'server': os.getenv('IMAP_SERVER', '')
 }
+DEFAULT_FOLDER = 'INBOX'
 
 class ImapHandler:
 
@@ -19,6 +21,21 @@ class ImapHandler:
         if not client_args.get('password', ''):
             raise Exception('The ImapHandler requires password to be provided in the client settings!')
 
+        if not client_args.get('server', ''):
+            raise Exception('The ImapHandler requires a server to be provided in the client settings!')
+
         self.email = client_args['email']
         self.password = client_args['password']
-    
+        self.server = client_args['server']
+
+    def get_message_ids(self, num_messages=1):
+        with IMAPClient(self.server, ssl=True) as client:
+            client.login(self.email, self.password)
+            client.select_folder(DEFAULT_FOLDER)
+
+            all_msg_ids = client.search(['ALL'])
+            print('All msg ids: ', all_msg_ids)
+
+            oldest_ids = all_msg_ids[:num_messages]
+            print('Oldest ids: ', oldest_ids)
+            return oldest_ids
--- test_gmail.py
+++ test_gmail.py
@@ -1,19 +1,32 @@
-from clients.gmail import GmailClient
+# from clients.gmail import GmailClient
 
-TOKEN_FILE = 'client_artefacts/gmail/token.json'
-CREDENTIALS_FILE = 'client_artefacts/gmail/credentials.json'
+# TOKEN_FILE = 'client_artefacts/gmail/token.json'
+# CREDENTIALS_FILE = 'client_artefacts/gmail/credentials.json'
 
-client = GmailClient(token_file=TOKEN_FILE, creds_file=CREDENTIALS_FILE)
+# client = GmailClient(token_file=TOKEN_FILE, creds_file=CREDENTIALS_FILE)
 
-res = client.list_emails()
+# res = client.list_emails()
 
-messages = res.get('messages', [])
+# messages = res.get('messages', [])
 
-for message in messages:
-    print('Message ID: ', message['id'])
-    print(message)
-    print('='*100)
+# for message in messages:
+#     print('Message ID: ', message['id'])
+#     print(message)
+#     print('='*100)
 
-print('Number of messages found: ', len(messages))
-print('Result size estimate: ', res.get('resultSizeEstimate'))
-print('Next page token: ', res.get('nextPageToken'))
+# print('Number of messages found: ', len(messages))
+# print('Result size estimate: ', res.get('resultSizeEstimate'))
+# print('Next page token: ', res.get('nextPageToken'))
+
+import os
+from clients.imap import ImapHandler
+
+client_args = {
+    'email': os.getenv('GMAIL_ACC'),
+    'password': os.getenv('GMAIL_APP_PASS'),
+    'server': os.getenv('GMAIL_IMAP_SERVER')
+}
+
+client = ImapHandler(client_args=client_args)
+
+client.get_message_ids(10)
